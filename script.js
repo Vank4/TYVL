99636dca08f7920721182271c00084894937e6d4
Vank4 <trandatvan0203@gmail.com>
2025-03-12 20:18:41 +0700

    up

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-fetch("head.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.head.innerHTML += data;
-  })
-  .catch((error) => console.error("Lỗi khi tải head.html:", error));
-
 function includeHTML() {
   document.getElementById("header").innerHTML = fetch("header.html")
     .then((response) => response.text())

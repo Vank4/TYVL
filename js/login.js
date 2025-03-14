72194e4cd1918383b0c03a12c82df4721fff85c3
Vank4 <trandatvan0203@gmail.com>
2025-03-15 01:55:11 +0700

    up

--- js/login.js
+++ js/login.js
@@ -1,6 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const registerButton = document.getElementById("register");
+  const loginButton = document.getElementById("login");
+  const container = document.getElementById("container");
+
+  if (registerButton && loginButton && container) {
+    registerButton.addEventListener("click", () => {
+      container.classList.add("right-panel-active");
+    });
+
+    loginButton.addEventListener("click", () => {
+      container.classList.remove("right-panel-active");
+    });
+  }
+});
+
 function loginUser() {
-  // Giả sử đăng nhập thành công
-  localStorage.setItem("loggedInUser", "true");
-  window.location.href = "home.html"; // Chuyển về trang chủ sau khi đăng nhập
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const storedUser = JSON.parse(localStorage.getItem("userAdmin"));
+
+  if (
+    storedUser &&
+    storedUser.username === username &&
+    storedUser.password === password
+  ) {
+    alert("Đăng nhập thành công!");
+    localStorage.setItem("loggedInUser", "true");
+    window.location.href = "home.html";
+  } else {
+    alert("Sai tên đăng nhập hoặc mật khẩu!");
+  }
 }
-s;
